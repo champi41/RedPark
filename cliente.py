aae450c728f68741e0a66f1d42a47868cb4a4c9a
oliam435 <alojamega4@gmail.com>
2024-04-22 18:54:43 -0400

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -10,23 +10,20 @@ class cliente:
         self.telefono = telefono
         self.correo = correo
         self.rut = rut
-    def crear_cliente(self, nombre, apellido, telefono, correo, rut,):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.telefono = telefono
-        self.correo = correo
-        self.rut = rut
-        nombre = input("Ingrese su nombre: ")
-        apellido = input("Ingrese su apellido: ")
-        telefono = input("Ingrese su telefono: ")
-        correo = input("Ingrese su correo: ")
-        rut = input("Ingrese su rut: ")
-        print("Nombre: ", nombre)
-        print("Apellido: ", apellido)
-
-
-        
-    
-    
-    
-    
+    @classmethod
+    def crear_cliente(cls):
+        nombre = input("Ingrese el nombre del cliente: ")
+        apellido = input("Ingrese el apellido del cliente: ")
+        rut = input("Ingrese el RUT del cliente: ")
+        telefono = input("Ingrese el teléfono del cliente: ")
+        correo = input("Ingrese el correo electrónico del cliente: ")
+        return cls(nombre, apellido, rut, telefono, correo)
+    def mostrar_cliente(self):
+        print("Información del cliente:")
+        print("Nombre:", self.nombre)
+        print("Apellido:", self.apellido)
+        print("RUT:", self.rut)
+        print("Teléfono:", self.telefono)
+        print("Correo electrónico:", self.correo)
+nuevo_cliente = cliente.crear_cliente()
+nuevo_cliente.mostrar_cliente()
