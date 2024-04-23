0dfd5d72a25cc5a265bd3345ce9f0db0acb3e4a1
Benjamín Tabilo <143561981+champi41@users.noreply.github.com>
2024-04-22 20:23:22 -0400

    Update ventanredticket.py

--- ventanredticket.py
+++ ventanredticket.py
@@ -2,7 +2,9 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 
 
 class Ui_RedTicket(object):
+    
     def setupUi(self, RedTicket):
+        
         RedTicket.setObjectName("RedTicket")
         RedTicket.resize(640, 480)
         RedTicket.setStyleSheet("background-color: rgb(0, 0, 90);\n"
@@ -16,6 +18,7 @@ class Ui_RedTicket(object):
         self.lblRedTicket.setFont(font)
         self.lblRedTicket.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.lblRedTicket.setObjectName("lblRedTicket")
+        
         self.lblDuracionTicket = QtWidgets.QLabel(parent=RedTicket)
         self.lblDuracionTicket.setGeometry(QtCore.QRect(160, 170, 90, 40))
         font = QtGui.QFont()
@@ -24,6 +27,7 @@ class Ui_RedTicket(object):
         self.lblDuracionTicket.setFont(font)
         self.lblDuracionTicket.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblDuracionTicket.setObjectName("lblDuracionTicket")
+        
         self.lblNombreTicket = QtWidgets.QLabel(parent=RedTicket)
         self.lblNombreTicket.setGeometry(QtCore.QRect(160, 230, 90, 40))
         font = QtGui.QFont()
@@ -32,6 +36,7 @@ class Ui_RedTicket(object):
         self.lblNombreTicket.setFont(font)
         self.lblNombreTicket.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblNombreTicket.setObjectName("lblNombreTicket")
+        
         self.lblPatenteTicket = QtWidgets.QLabel(parent=RedTicket)
         self.lblPatenteTicket.setGeometry(QtCore.QRect(160, 290, 90, 40))
         font = QtGui.QFont()
@@ -40,6 +45,7 @@ class Ui_RedTicket(object):
         self.lblPatenteTicket.setFont(font)
         self.lblPatenteTicket.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblPatenteTicket.setObjectName("lblPatenteTicket")
+        
         self.lblDuracionDatos = QtWidgets.QLabel(parent=RedTicket)
         self.lblDuracionDatos.setGeometry(QtCore.QRect(260, 170, 251, 40))
         font = QtGui.QFont()
@@ -49,6 +55,7 @@ class Ui_RedTicket(object):
         self.lblDuracionDatos.setText("")
         self.lblDuracionDatos.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblDuracionDatos.setObjectName("lblDuracionDatos")
+        
         self.lblNombreDatos = QtWidgets.QLabel(parent=RedTicket)
         self.lblNombreDatos.setGeometry(QtCore.QRect(260, 230, 251, 40))
         font = QtGui.QFont()
@@ -58,6 +65,7 @@ class Ui_RedTicket(object):
         self.lblNombreDatos.setText("")
         self.lblNombreDatos.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblNombreDatos.setObjectName("lblNombreDatos")
+        
         self.lblPatenteDatos = QtWidgets.QLabel(parent=RedTicket)
         self.lblPatenteDatos.setGeometry(QtCore.QRect(260, 290, 251, 40))
         font = QtGui.QFont()
@@ -67,6 +75,7 @@ class Ui_RedTicket(object):
         self.lblPatenteDatos.setText("")
         self.lblPatenteDatos.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeading|QtCore.Qt.AlignmentFlag.AlignLeft|QtCore.Qt.AlignmentFlag.AlignVCenter)
         self.lblPatenteDatos.setObjectName("lblPatenteDatos")
+        
         self.lblAviso2 = QtWidgets.QLabel(parent=RedTicket)
         self.lblAviso2.setGeometry(QtCore.QRect(130, 390, 411, 21))
         font = QtGui.QFont()
@@ -76,17 +85,13 @@ class Ui_RedTicket(object):
         self.lblAviso2.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.lblAviso2.setObjectName("lblAviso2")
 
-        self.retranslateUi(RedTicket)
-        QtCore.QMetaObject.connectSlotsByName(RedTicket)
 
-    def retranslateUi(self, RedTicket):
-        _translate = QtCore.QCoreApplication.translate
-        RedTicket.setWindowTitle(_translate("RedTicket", "Dialog"))
-        self.lblRedTicket.setText(_translate("RedTicket", "<html><head/><body><p>Red<span style=\" color:#ff0004;\">Ticket</span></p></body></html>"))
-        self.lblDuracionTicket.setText(_translate("RedTicket", "Duración:"))
-        self.lblNombreTicket.setText(_translate("RedTicket", "Nombre:"))
-        self.lblPatenteTicket.setText(_translate("RedTicket", "Patente:"))
-        self.lblAviso2.setText(_translate("RedTicket", "Puede cerrar las ventanas"))
+        RedTicket.setWindowTitle("Dialog")
+        self.lblRedTicket.setText("<html><head/><body><p>Red<span style=\" color:#ff0004;\">Ticket</span></p></body></html>")
+        self.lblDuracionTicket.setText("Duración:")
+        self.lblNombreTicket.setText("Nombre:")
+        self.lblPatenteTicket.setText("Patente:")
+        self.lblAviso2.setText("Puede cerrar las ventanas")
 
 
 if __name__ == "__main__":
