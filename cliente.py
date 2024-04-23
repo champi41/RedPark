fc559903e6f0c0229128348f8a45a476f04721cf
Benjamín Tabilo <143561981+champi41@users.noreply.github.com>
2024-04-22 20:24:13 -0400

    Create cliente.py

---
+++ cliente.py
@@ -0,0 +1,11 @@
+class cliente:
+    
+    nombre : str
+    patente: str
+    
+    def __init__(self, nombre, patente):
+        
+        self.nombre = nombre
+        self.patente = patente
+        
+        
