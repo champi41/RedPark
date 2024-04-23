b29990a26854c089664b92e69e198c4a592dad26
Benjamín Tabilo <143561981+champi41@users.noreply.github.com>
2024-04-22 23:08:38 -0400

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,13 @@
+class ticket:
+    
+    nombreConductor: str
+    patenteVehiculo: str
+    duracion: str
+    precio: str
+    
+    def __init__(self, nombreConductor, patenteVehiculo, duracion, precio):
+        
+        self.nombreConductor = nombreConductor
+        self.patenteVehiculo = patenteVehiculo
+        self.duracion = duracion
+        self.precio = precio
