6ba3c2db1d8e9f8bb857852a6bf3625aeff1f7a3
oliam435 <121459246+oliam435@users.noreply.github.com>
2024-05-27 19:58:24 -0400

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -16,19 +16,16 @@ class ticket:
         self.rut = rut
         #aca tendria que ir el lugar en la amtriz que falta hacer aun
         
-    def Crear_Boleta(){
+    def Crear_Boleta(self):
         ticket_aux = {
-            "Rut": ticket.rut,
-            "Nombre": ticket.nombre,
-            "Patente": ticket.patenteVehiculo,
-            "tiempo": ticket.duracion,
-            "valor": ticket.precio,
-            #"lugar": ticket.lugar porque la matriz aun no esta hecha
+            "Rut": self.rut,
+            "Nombre": self.nombreConductor,
+            "Patente": self.patenteVehiculo,
+            "tiempo": self.duracion,
+            "valor": self.precio,
         }
         ticket_json = json.dumps(ticket_aux)
-        with open("ticket.json", "w") as json_file:
+        os.makedirs("Boletas", exist_ok=True)
+        file_path = os.path.join("Boletas", "ticket.json")
+        with open(file_path, "w") as json_file:
             json_file.write(ticket_json)
-    }
-    #preguntar al profe si en la funcion open es mejor el "w" o el "a" o el "r+" no se cual estaria mejor
-#preguntar igual una forma de que las boletas vallan guardandose con distintos numeros o si el mismo explorador de archivos va creando
-#distintos numeros en los ticket(como cuando hay un mismo archivo el ordenador hace archivo archivo(2)
