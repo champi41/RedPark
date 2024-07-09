9a3ea1262d5089fffe4799a1b755a4abd43bfcaf
Benjamín Tabilo <143561981+champi41@users.noreply.github.com>
2024-07-09 10:48:10 -0400

    Update resumen.py

--- resumen.py
+++ resumen.py
@@ -1,18 +1,17 @@
-class Ui_Resumen(QtWidgets.QWidget):
-     
-    def __init__(self, parent):
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QFileDialog
+import sys
+import json
+
+
+class Ui_Resumen(QtWidgets.QDialog):
         
-        super().__init__(parent)
-        self.setupResumen()   
-           
-    def setupResumen(self):
-            
-        self.setObjectName("Resumen")
-        self.resize(640, 480)
-        self.setStyleSheet("background-color: rgb(0, 0, 90);\n"
+    def setupResumen(self, Resumen):
+        Resumen.setObjectName("Resumen")
+        Resumen.resize(640, 480)
+        Resumen.setStyleSheet("background-color: rgb(0, 0, 90);\n"
 "color: rgb(255, 255, 255);")
-        
-        self.lblResumen = QtWidgets.QLabel(parent=self)
+        self.lblResumen = QtWidgets.QLabel(parent=Resumen)
         self.lblResumen.setGeometry(QtCore.QRect(160, 50, 320, 50))
         font = QtGui.QFont()
         font.setFamily("Calibri")
@@ -20,34 +19,13 @@ class Ui_Resumen(QtWidgets.QWidget):
         self.lblResumen.setFont(font)
         self.lblResumen.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.lblResumen.setObjectName("lblResumen")
-
-        self.lblUsuario = QtWidgets.QLabel(parent=self)
-        self.lblUsuario.setGeometry(QtCore.QRect(20, 150, 75, 50))
-        font = QtGui.QFont()
-        font.setFamily("Calibri")
-        font.setPointSize(14)
-        self.lblUsuario.setFont(font)
-        self.lblUsuario.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.lblUsuario.setObjectName("lblUsuario")
-        self.lblUsuario.setText("Usuario: ")
-        
-        self.lblNombreUsu = QtWidgets.QLabel(parent=self)
-        self.lblNombreUsu.setGeometry(QtCore.QRect(100, 150, 75, 50))
-        font = QtGui.QFont()
-        font.setFamily("Calibri")
-        font.setPointSize(14)
-        self.lblNombreUsu.setFont(font)
-        self.lblNombreUsu.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.lblNombreUsu.setObjectName("lblNombreUsu")
-        self.lblNombreUsu.setText("Nombre")
-        
-        self.btnAtras = QtWidgets.QPushButton(parent=self)
+        self.btnAtras = QtWidgets.QPushButton(parent=Resumen)
         self.btnAtras.setGeometry(QtCore.QRect(20, 410, 161, 41))
         self.btnAtras.setStyleSheet("background-color: rgb(255, 0, 0);\n"
 "border-radius: 20px\n"
 "")
         self.btnAtras.setObjectName("btnAtras")
-        self.btnAbrirRes = QtWidgets.QPushButton(parent=self)
+        self.btnAbrirRes = QtWidgets.QPushButton(parent=Resumen)
         self.btnAbrirRes.setGeometry(QtCore.QRect(220, 230, 201, 51))
         font = QtGui.QFont()
         font.setPointSize(14)
@@ -56,8 +34,37 @@ class Ui_Resumen(QtWidgets.QWidget):
 "border-radius: 20px\n"
 "")
         self.btnAbrirRes.setObjectName("btnAbrirRes")
+        self.btnAbrirRes.clicked.connect(self.abrir_json)
+        
+        self.retranslateUi(Resumen)
+        QtCore.QMetaObject.connectSlotsByName(Resumen)
 
-        self.setWindowTitle("Resumen")
-        self.lblResumen.setText("Resumen de ventas")
-        self.btnAtras.setText("Atras")
-        self.btnAbrirRes.setText("Abrir resumen")
+    def retranslateUi(self, Resumen):
+        _translate = QtCore.QCoreApplication.translate
+        Resumen.setWindowTitle(_translate("Resumen", "Resumen"))
+        self.lblResumen.setText(_translate("Resumen", "Resumen de ventas"))
+        self.btnAtras.setText(_translate("Resumen", "Atras"))
+        self.btnAbrirRes.setText(_translate("Resumen", "Abrir resumen"))
+        
+    
+    def abrir_json(self):
+            
+        file_dialog = QFileDialog(self)
+        file_dialog.setNameFilter("JSON files (BaseDato.json)")
+        file_dialog.selectNameFilter("JSON files (BaseDato.json)")
+        
+        if file_dialog.exec():
+            file_path = file_dialog.selectedFiles()[0]
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                # Aquí puedes procesar los datos del archivo JSON como lo necesites
+                print(data)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Resumen = Ui_Resumen()
+    ui = Ui_Resumen()
+    ui.setupResumen(Resumen)
+    Resumen.show()
+    sys.exit(app.exec())
