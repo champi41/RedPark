c3b314ffdc4684e744eb80bd220bc69beec7b600
oliam435 <alojamega4@gmail.com>
2024-07-09 11:14:39 -0400

    Create Resumenv2.py

---
+++ Resumenv2.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+BaseDato = "BaseDato.json"
+if not os.path.exists(BaseDato):
+    with open(BaseDato, 'w') as ArchivodeDatos:
+        json.dump({}, ArchivodeDatos)
+        
+def LeerBD():
+        with open(BaseDato, 'r') as ArchivodeDatos:
+            return json.load(ArchivodeDatos)
+
+def EscribirBD(data):
+    with open(BaseDato, 'w') as ArchivodeDatos:
+        json.dump(data, ArchivodeDatos, indent=4)
+        
+def LeerTodo():
+    return LeerBD()
+
+def Crear_Actualizar(key, value):
+    data = LeerBD()
+    data[key] = value
+    EscribirBD(data)
+
+def LeerArchivo(key):
+    data = LeerBD()
+    return data.get(key, None)
+
+
+Crear_Actualizar("1", {"Nombre": 'Alice', 'Edad': 30})
+Crear_Actualizar('2', {'Nombre': 'Bob', 'Edad': 25})
+
+print(LeerArchivo('1'))
